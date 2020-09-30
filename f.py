178998407534d07f17e4523e7ec083b96a7184e7
poojakumari29 <70048572+poojakumari29@users.noreply.github.com>
2020-09-30 08:35:07 +0530

    Add files via upload

---
+++ f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print("content-type: text/html\n")
+
+import cgi
+import subprocess
+
+form = cgi.FieldStorage()
+cmd =form.getvalue("c")
+
+output = subprocess.getoutput(cmd)
+print(output)
